a36e25b98866c7444d3f1e86f287f3a0c6fed432
dean131 <deanabdillah@gmail.com>
2024-12-22 17:51:50 +0000

    chore: Set up Docker configuration and update environment variables for application and database

--- src/route/public-api.js
+++ src/route/public-api.js
@@ -2,6 +2,9 @@ import express from "express";
 import userController from "../controller/user-controller.js";
 
 const publicRouter = new express.Router();
+publicRouter.get("/", (req, res) => {
+  res.send("<h1>Hello World</h1>");
+});
 publicRouter.post("/api/users", userController.register);
 publicRouter.post("/api/users/login", userController.login);
 
